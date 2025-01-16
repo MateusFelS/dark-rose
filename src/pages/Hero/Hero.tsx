630d6c600243f1ac2a7d2d3523d121045dc6289a
Jh0wjso <jhonatansilverio1258@gmail.com>
2025-01-16 20:58:46 -0300

    Code more clean

---
+++ src/pages/Hero/Hero.tsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+const Hero = () => {
+  return (
+    <div className="heroContainer">
+      <div className="header">
+        <p className="text-lg">+55 12 3456-7890</p>
+        <h1 className="headerTitle">Dark Rose</h1>
+        <hr className="absolute w-[95vw]" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
